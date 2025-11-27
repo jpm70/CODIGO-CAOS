9594b9d488218403849295e8ab6cda90008e2705
José Pérez <jpm70a@gmail.com>
2025-11-27 15:25:34 +0100

    Add files via upload

---
+++ external_secrets.js
@@ -0,0 +1,14 @@
+/**
+ * external_secrets.js
+ * * Este archivo está enlazado en reto10.html.
+ * Es una práctica común dejar comentarios o variables sin usar
+ * que contienen información sensible.
+ */
+
+// Esta variable NO se usa, pero contiene la segunda parte de la bandera.
+const SECRET_PART_2 = "3XPUE5T0S"; // <-- ¡La segunda pieza del rompecabezas!
+
+// Función de ejemplo (no usada en el HTML)
+function initSecurityCheck() {
+    console.log("Sistema de seguridad inicializado.");
+}
